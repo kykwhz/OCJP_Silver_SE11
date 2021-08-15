af00f637dfbdec76fa5ee4f5effa6b4dffe96380
kykwhz <kykwhz@gmail.com>
2021-08-15 16:24:45 +0900

    06-17を追加。

---
+++ OCJP_Silver_SE11/src/Sample.java
@@ -0,0 +1,24 @@
+interface Test1 {
+	void foo();
+}
+
+interface Test2 {
+	void bar();
+}
+
+abstract class Test3 {
+	public abstract void foo();
+}
+
+public class Sample extends Test3 implements Test1, Test2 {
+	public void foo() {
+	}
+
+	public void bar() {
+	}
+	
+	
+	public static void main(String[] args) {
+		new Sample();
+	}
+}
